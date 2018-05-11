6755ac64a815849a237708361e7f43ab8ddb0579
Ammar Tareen <tareen@qbphysicsammar.cshl.edu>
2018-05-11 09:59:03 -0400

    Mocking mpathic in conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,6 +82,7 @@ extensions = [
 autodoc_mock_imports = ['cStringIO',
                         'cvxopt',
                         'Bio',
+                        'mpathic'
                         ]
 
 '''
