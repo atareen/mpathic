578a30d6311b555caf6529ee202874bca534edda
Ammar Tareen <atareen64@gmail.com>
2018-05-30 22:11:10 -0400

    Updating conf.py to not use override.css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -162,9 +162,9 @@ html_theme = 'bizstyle'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': ['_static/overrides.css'],
-}
+#html_context = {
+#    'css_files': ['_static/overrides.css'],
+#}
 
 
 # Custom sidebar templates, must be a dictionary that maps document names
